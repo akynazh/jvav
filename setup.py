3a709e106a25604ca7f9fe8e6e9bf350eaa9f2a5
jiangzhh <akynazh@qq.com>
2023-07-18 18:37:14 +0800

    version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="Jvav",
-    version="1.2.31",
+    version="1.2.33",
     description="Useful tools for Jav.",
     long_description=readme,
     long_description_content_type="text/markdown",
